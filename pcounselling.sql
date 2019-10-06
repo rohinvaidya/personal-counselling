da9644bd77dd2e0bebe24a2bf47bf4ddf533bd27
Rohin Vaidya <rohinvaidya@users.noreply.github.com>
2019-10-06 18:27:35 +0530

    admin-interface

---
+++ pcounselling.sql
@@ -0,0 +1,150 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 06, 2019 at 01:58 PM
+-- Server version: 10.1.28-MariaDB
+-- PHP Version: 7.3.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `counselling`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `colleges`
+--
+
+CREATE TABLE `colleges` (
+  `college_id` int(11) NOT NULL,
+  `college_name` varchar(255) NOT NULL,
+  `stream` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `contact_no` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `website` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `colleges`
+--
+
+INSERT INTO `colleges` (`college_id`, `college_name`, `stream`, `city`, `address`, `description`, `image`, `contact_no`, `email`, `website`) VALUES
+(1, 'jai hind', 'arts, science, commerce', 'mumbai ', 'churchgate ', 'dljanckbslkvbifbvpajb cphb', 'jaihing.jpeg', '745685412', 'jaihind@jaihin.com', 'www.jaihind.in'),
+(2, 'KC college', 'commerce, science', 'mumbai', 'fort', 'asdjnxkasvdouabdlvco', 'kc.jpeg', 'uv567ui', 'kc@gmail.com', 'www.kc.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `courses`
+--
+
+CREATE TABLE `courses` (
+  `course_id` int(11) NOT NULL,
+  `course_name` varchar(255) NOT NULL,
+  `subcourse_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subcourses`
+--
+
+CREATE TABLE `subcourses` (
+  `subcourse_id` int(11) NOT NULL,
+  `subcourses` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `contact_no` varchar(255) NOT NULL,
+  `preferences` varchar(255) DEFAULT NULL,
+  `role` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `first_name`, `last_name`, `email`, `password`, `contact_no`, `preferences`, `role`) VALUES
+(1, 'Shalva', 'Thakurdesai', 'shalva@gmail.com', 'shalva@123', '9820098200', NULL, 'admin'),
+(2, 'Rohin', 'Vaidya', 'rohin@gmail.com', 'rohin@123', '9876543211', NULL, 'counsellor'),
+(3, 'Shubham', 'Vira', 'shubham@gmail.com', 'shubham@123', '9632587410', 'science', 'client'),
+(4, 'Alex', 'Solanki', 'alex@gmail.com', 'alex@123', '6547893210', 'commerce', 'client'),
+(5, 'Abdullah', 'Khan', 'abdulha@gmail.com', 'abdullah@1243', '9876543210', 'arts', 'client');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `colleges`
+--
+ALTER TABLE `colleges`
+  ADD PRIMARY KEY (`college_id`);
+
+--
+-- Indexes for table `courses`
+--
+ALTER TABLE `courses`
+  ADD PRIMARY KEY (`course_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `colleges`
+--
+ALTER TABLE `colleges`
+  MODIFY `college_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `courses`
+--
+ALTER TABLE `courses`
+  MODIFY `course_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
